3a819f188dd4a63aadb46f581f36e3304e294cda
Haçik Ulu <hacikd2@gmail.com>
2024-12-14 16:49:08 +0300

    credit-calculator app.

--- credit-calculator/src/app/app.component.ts
+++ credit-calculator/src/app/app.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [FormsModule],
+  template: `
+  <h1>Kredi Hesaplama</h1>
+  <div>
+    <label>Kredi Tutarı</label>
+    <input [(ngModel)]="krediTutari">
+  </div>
+  <div>
+    <label>Taksit Sayısı</label>
+    <select [(ngModel)]="taksitSayisi">
+    @for (data of taksitler;track data) 
+        {
+          <option>{{data}}</option>
+        }    </select>
+  </div>
+  <div>
+    <button (click)="hesapla()">Hesapla</button>
+  </div>
+  <hr>
+  <h1>{{ result }}</h1>
+  <hr>
+  <table>
+    <thead>
+      <tr>
+        <th>Taksit</th>
+        <th>Taksit Tutarı</th>
+        <th>Kalan Geri Ödeme</th>
+      </tr>
+    </thead>
+    <tbody>
+      @for(data of odemePlani; track data){
+        <tr>
+          <td>{{$index+1}}</td>
+          <td>{{data.taksitTutari}}</td>
+          <td>{{data.kalanGeriOdeme}}</td>
+        </tr>
+      }      
+    </tbody>
+  </table>
+  `
 })
 export class AppComponent {
-  title = 'credit-calculator';
+  krediTutari: number = 0;
+  taksitSayisi: number = 3;
+
+  taksitler: number[] = [3, 6, 9, 12, 24];
+
+  result: string = "";
+
+  odemePlani: { taksitTutari: number; kalanGeriOdeme: number }[] = [];
+
+  hesapla() {
+    const taksitTutari: number = (this.krediTutari / this.taksitSayisi) * 1.29;
+    let toplamGeriOdeme: number = taksitTutari * this.taksitSayisi;
+
+    this.result = `Taksit Tutarı: ${taksitTutari} 
+    Taksit Sayısı: ${this.taksitSayisi} 
+    Toplam Geri Ödeme: ${toplamGeriOdeme}`;
+
+    this.odemePlani = [];
+    for (let i = 0; i < this.taksitSayisi; i++) {
+      toplamGeriOdeme -= taksitTutari;
+
+      const data = {
+        taksitTutari: taksitTutari,
+        kalanGeriOdeme: toplamGeriOdeme
+      };
+
+      this.odemePlani.push(data);
+    }
+  }
 }
